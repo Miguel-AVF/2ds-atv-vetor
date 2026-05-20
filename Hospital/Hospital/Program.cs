09c7c9acc973a583f74d8bcdc87b35ec47aa9807
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-05-19 22:13:24 -0300

    Programa Principal

---
+++ Hospital/Hospital/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string op = "w";
+            Fila fila = new Fila();
+
+            while (op != "q" && op != "Q")
+            {
+                Console.Clear();              //Menu
+
+                Console.WriteLine("===================================");
+                Console.WriteLine(" SISTEMA DE FILA DO HOSPITAL ");
+                Console.WriteLine("===================================");
+                Console.WriteLine("1 - Cadastrar paciente");
+                Console.WriteLine("2 - Listar fila");
+                Console.WriteLine("3 - Atender paciente");
+                Console.WriteLine("4 - Alterar cadastro");
+                Console.WriteLine("q - Sair");
+                Console.WriteLine("===================================");
+                Console.Write("Escolha uma opção: ");
+
+                op = Console.ReadLine();
+                switch (op)                //opções e metodos 
+                {
+                    case "1":
+                        fila.Cadastro();  
+                        break;
+
+                    case "2":
+                        fila.Listar();
+                        break;
+
+                    case "3":
+                        fila.Atender();
+                        break;
+
+                    case "4":
+                        fila.Alterar();
+                        break;
+
+                    case "q":
+                    case "Q":
+                        Console.WriteLine("\n...Encerrando Programa...");
+                        break;
+
+                    default:
+                        Console.WriteLine("//--Comando Inválido--//");
+                        break;
+                }
+
+                if (op != "q" && op != "Q")
+                {
+                    Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
