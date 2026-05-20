3429d0eb7aa356fefc6fe170568d92734009e31b
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-05-19 22:20:27 -0300

    Classe Paciente

---
+++ Hospital/Hospital/Paciente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Paciente : Pessoa
+    {
+        //Dados
+        public bool Preferencial;
+
+        public Paciente(string nome, int idade)//Criando variaveis
+        {
+            Nome = nome;
+            Idade = idade;
+            Preferencial = idade >= 60; //Se a idade for maior ou igual 60 é preferencial
+        }
+    }
+}
